8171d093e585d29685acee0ab2d8aa349af74f60
Erkan Erdem <erkanmdr@gmail.com>
2015-02-05 19:41:18 +0200

    first initial commit

--- kutu/gitcommitter.sh
+++ kutu/gitcommitter.sh
@@ -4,6 +4,13 @@
 #File copy...
 
 cp -avr /home/erkanmdr/kutu /home/erkanmdr/programlar/git/HELYXBox
-cp -avr /home/erkanmdr/.i3 /home/erkanmdr/programlar/git/HELYXBox
-cp /home/erkanmdr/.conkyrc  /home/erkanmdr/programlar/git/HELYXBox
-cp /home/erkanmdr/.config/dmenu-extended/config/dmenuExtended_preferences.txt  /home/erkanmdr/programlar/git/HELYXBox/.config/dmenu-extended/config/
+cp -avr /home/erkanmdr/.i3/* /home/erkanmdr/programlar/git/HELYXBox/i3
+cp /home/erkanmdr/.conkyrc  /home/erkanmdr/programlar/git/HELYXBox/conkyrc
+cp /home/erkanmdr/.config/dmenu-extended/config/dmenuExtended_preferences.txt  /home/erkanmdr/programlar/git/HELYXBox/config/dmenu-extended/config/
+
+cd /home/erkanmdr/programlar/git/HELYXBox/
+git add *
+echo "commit message: "
+read cm
+git commit -m "$cm"
+git push origin master
