aa36f2e25a43e50d60f9704513e39d66fd8b1b9a
Erkan Erdem <erkanmdr@gmail.com>
2015-02-05 19:35:18 +0200

    Commit message

---
+++ kutu/gitcommitter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Git committer for HELYXBox by ErkanMDR
+
+#File copy...
+
+cp -avr /home/erkanmdr/kutu /home/erkanmdr/programlar/git/HELYXBox
+cp -avr /home/erkanmdr/.i3 /home/erkanmdr/programlar/git/HELYXBox
+cp /home/erkanmdr/.conkyrc  /home/erkanmdr/programlar/git/HELYXBox
+cp /home/erkanmdr/.config/dmenu-extended/config/dmenuExtended_preferences.txt  /home/erkanmdr/programlar/git/HELYXBox/.config/dmenu-extended/config/
---
+++ kutu/havadurumu.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#AccuWeather (r) RSS weather tool for conky
+#
+#USAGE: weather.sh <locationcode>
+#
+#(c) Michael Seiler 2007
+
+METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
+
+if [ -z $1 ]; then
+    echo
+    echo "USAGE: weather.sh <locationcode>"
+    echo
+    exit 0;
+fi
+
+#curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+
+out=`curl --connect-timeout 30 -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'`
+
+#echo $out
+
+cond=`echo $out | cut -d':' -f 1`
+temp=`echo $out | cut -d':' -f 2`
+
+
+case "$cond" in
+'Fog')
+    echo -n "sisli"
+;;
+'Snow')
+    echo -n "karlı"
+;;
+'Cloudy')
+    echo -n "bulutlu"
+;;
+'Sunny')
+   echo -n "güneşli"
+;;
+'T-Storms')
+   echo -n "fırtınalı"
+;;
+'Mostly Cloudy')
+   echo -n "parçalı bulutlu"
+;;
+'Rain')
+   echo -n "yağmurlu"
+;;
+*)
+   echo -n  $cond'\c'
+esac
+
+
+echo -n "$temp"
